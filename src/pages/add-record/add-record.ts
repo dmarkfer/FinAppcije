8f933d6cfe3c91dbedfc403dc573921903ce60c0
Domagoj Markota <domagoj.markota@fer.hr>
2018-05-23 06:09:13 +0200

    Adding new records implemented

--- src/pages/add-record/add-record.ts
+++ src/pages/add-record/add-record.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+import { Toast } from '@ionic-native/toast';
 
 
 @IonicPage()
@@ -21,15 +22,41 @@ export class AddRecordPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private sqlite: SQLite
+    private sqlite: SQLite,
+    private toast: Toast
   ) {
   }
 
-  
+
   saveData(): void {
     this.sqlite.create({
       name: 'finappcije.db',
       location: 'default'
+    }).then((db: SQLiteObject) => {
+      db.executeSql('INSERT INTO record VALUES(NULL,?,?,?,?)', [this.data.date, this.data.type, this.data.description, this.data.amount])
+        .then(res => {
+          console.log(res);
+          this.toast.show('Data saved', '5000', 'center').subscribe(
+            toast => {
+              this.navCtrl.popToRoot();
+            }
+          );
+        })
+        .catch(e => {
+          console.log(e);
+          this.toast.show(e, '5000', 'center').subscribe(
+            toast => {
+              console.log(toast);
+            }
+          );
+        });
+    }).catch(e => {
+      console.log(e);
+      this.toast.show(e, '5000', 'center').subscribe(
+        toast => {
+          console.log(toast);
+        }
+      );
     });
   }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 import { RecordsPage } from '../records/records';
 import { ChartsPage } from '../charts/charts';
 import { AddRecordPage } from '../add-record/add-record';
+import { Tabs } from 'ionic-angular';
 
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit, AfterViewInit {
+
+  @ViewChild("tabs") tabs: Tabs;
 
   recordsTabRoot = RecordsPage;
   chartsTabRoot = ChartsPage;
@@ -36,9 +39,13 @@ export class HomePage {
   }
 
 
-  /*ngOnInit(): void {
-    this.selectedRecordTypes = this.recordTypes[3];
-  }*/
+  ngOnInit(): void {
+    //this.selectedRecordTypes = this.recordTypes[3];
+  }
+
+  ngAfterViewInit(): void {
+    //this.tabs.select(0);
+  }
 
   ionViewDidLoad(): void {
     this.getRecords();
